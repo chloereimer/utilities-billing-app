36abb9e7a5767d36af30d2dfc08fbaeab478c2a4
Chloe Reimer <chloe.reimer@gmail.com>
2014-12-04 07:33:46 -0500

    gets started on authentication

--- src/java/com/mohawkcollege/cr/Account.java
+++ src/java/com/mohawkcollege/cr/Account.java
@@ -12,6 +12,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -53,6 +55,9 @@ public class Account implements Serializable {
     @NotNull
     @Column(name = "PRIVILEGE")
     private int privilege;
+    @JoinColumn(name = "METER_ID", referencedColumnName = "ID")
+    @ManyToOne
+    private Meter meterId;
 
     public Account() {
     }
@@ -98,6 +103,14 @@ public class Account implements Serializable {
 
     public void setPrivilege(int privilege) {
         this.privilege = privilege;
+    }
+    
+    public Meter getMeterId(){
+        return meterId;
+    }
+    
+    public void setMeterId(Meter meterId) {
+        this.meterId = meterId;
     }
 
     @Override
--- src/java/com/mohawkcollege/cr/AuthenticationBean.java
+++ src/java/com/mohawkcollege/cr/AuthenticationBean.java
@@ -5,21 +5,129 @@
  */
 package com.mohawkcollege.cr;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import sun.misc.BASE64Encoder;
 
 /**
  *
  * @author chloe
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class AuthenticationBean {
+    
+    private String username;
+    private String password;
+    private String streetNumber;
+    private String streetName;
+    private String meterId;
+    private int privilege;
+    @PersistenceContext(unitName = "utilities-billing-appPU")
+    private EntityManager em;
+    @Resource
+    private javax.transaction.UserTransaction utx;
 
-    /**
-     * Creates a new instance of AuthenticationBean
-     */
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getMeterId() {
+        return meterId;
+    }
+
+    public void setMeterId(String meterId) {
+        this.meterId = meterId;
+    }
+
+    public int getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(int privilege) {
+        this.privilege = privilege;
+    }
+
     public AuthenticationBean() {
+    }
+    
+    public String doRegistration(){
+        
+        String hash = null;
+        
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA");
+            md.update(password.getBytes("UTF-8"));
+            byte raw[] = md.digest();
+            hash = (new BASE64Encoder()).encode(raw);
+        } catch (Exception ex) {
+            Logger.getLogger(AuthenticationBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        Meter meter = (Meter) (em.createNamedQuery("Meter.findById").setParameter( "id", "Q34825" ).getSingleResult());
+        Account account = new Account();
+        account.setUsername("SomeUser");
+        account.setPassword(hash);
+        account.setPrivilege(1);
+        account.setMeterId(meter);
+        
+        boolean committed = false;
+        try{
+            utx.begin();
+            em.persist(account);
+            utx.commit();
+            committed = true;
+        } catch( Exception e ){
+        }
+        
+        return "admin/index?faces-redirect=true";
+    }
+
+    public void persist(Object object) {
+        try {
+            utx.begin();
+            em.persist(object);
+            utx.commit();
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            throw new RuntimeException(e);
+        }
     }
     
 }
