5e26d78d875837c6a6f3f7a341ad7aa42657ac11
Chloe Reimer <chloe.reimer@gmail.com>
2014-12-03 23:30:14 -0500

    fleshes out views/css and adds auth bean

---
+++ src/java/com/mohawkcollege/cr/AuthenticationBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mohawkcollege.cr;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author chloe
+ */
+@ManagedBean
+@RequestScoped
+public class AuthenticationBean {
+
+    /**
+     * Creates a new instance of AuthenticationBean
+     */
+    public AuthenticationBean() {
+    }
+    
+}
